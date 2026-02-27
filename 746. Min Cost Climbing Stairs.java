9f99fe6111d170b21c0928d22eacd9771aa84ff4
Priyanshu Chakole <150071919+priyanshu4040@users.noreply.github.com>
2026-02-27 17:28:48 +0530

    Update 746. Min Cost Climbing Stairs.java

--- 746. Min Cost Climbing Stairs.java
+++ 746. Min Cost Climbing Stairs.java
@@ -1,3 +1,4 @@
+//Starting from 0th position
 class Solution {
 
     static int minCost(int idx, int[] cost, int[] dp) {
@@ -15,3 +16,71 @@ class Solution {
         return Math.min(minCost(0, cost, dp), minCost(1, cost, dp));
     }
 }
+
+//Starting from Nth position
+class Solution {
+
+    static int solve(int[] cost,int[] dp, int n) {
+        if(n == 0)
+            return cost[0];
+        else if(n == 1)
+            return cost[1];
+
+        if(dp[n] != -1)
+            return dp[n];
+
+         dp[n] = Math.min(solve(cost,dp, n-1), solve(cost,dp, n-2))+cost[n];
+        return dp[n];
+    }
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        int ans = Math.min(solve(cost,dp,n-1), solve(cost,dp,n-2));
+        return ans;
+    }
+}
+
+//Bottom-Up approach(Tabulation)
+class Solution {
+
+    static int solve(int[] cost, int n) {
+        int[] dp = new int[n+1];
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+
+        for(int i = 2; i < n; i++) {
+            dp[i] = Math.min(dp[i-1], dp[i-2])+ cost[i];
+        }
+        return Math.min(dp[n-1], dp[n-2]);
+    }
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        
+        int ans = solve(cost, n);
+        return ans;
+    }
+}
+
+//Space Optimization
+    class Solution {
+
+        static int solve(int[] cost, int n) {
+            int prev1 = cost[0];
+            int prev2 = cost[1];
+            int curr = 0;
+
+            for(int i = 2; i < n; i++) {
+                curr = Math.min(prev1,prev2)+cost[i];
+                prev1 = prev2;
+                prev2 = curr;
+            }
+            return Math.min(prev1, prev2);
+        }
+        public int minCostClimbingStairs(int[] cost) {
+            int n = cost.length;
+            
+            int ans = solve(cost, n);
+            return ans;
+        }
+    }
