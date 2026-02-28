ab966094ac1830b7d6e688180a3b0071c2fa1189
Priyanshu Chakole <150071919+priyanshu4040@users.noreply.github.com>
2026-02-28 18:30:33 +0530

    Add solution for longest palindrome problem
    Implement solution to find the length of the longest palindrome that can be built with the given string.

---
+++ 409. Longest Palindrome.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int longestPalindrome(String s) {
+        int[] count = new int[128];
+
+        for(char ch : s.toCharArray()){
+            count[ch]++;
+        }
+        int res = 0;
+
+        for(int i = 0; i < 128; i++) {
+            int val = count[i];
+            //If the value is odd, it will consider even
+            //E.g : if value is 7, it will consider 6.
+            res = res + (val/2)*2;
+            if(res % 2 == 0 && val % 2 == 1)
+                res++;
+        }
+
+        return res;
+    }
+}
