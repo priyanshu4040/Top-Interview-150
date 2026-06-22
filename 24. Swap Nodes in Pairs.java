787ad37b9b644301f42846b46a1f56c7129b58c0
Priyanshu Chakole <150071919+priyanshu4040@users.noreply.github.com>
2026-06-22 21:39:23 +0530

    Implement swapPairs method to swap nodes

---
+++ 24. Swap Nodes in Pairs.java
@@ -0,0 +1,13 @@
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if(head == null || head.next == null) return head;
+
+        ListNode first = head, second = head.next;
+
+        first.next = swapPairs(second.next);
+        second.next = first;
+
+        return second;
+        
+    }
+}
